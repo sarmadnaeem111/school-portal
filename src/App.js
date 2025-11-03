f8b32072140b1b48e473e5357dfc4ef72d6c1ff5
sarmadnaeem111 <sarmadnaeem111@gmail.com>
2025-11-03 16:30:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,18 +10,6 @@ import ParentDashboard from './components/parent/ParentDashboard';
 import StudentDashboard from './components/student/StudentDashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import TransportDashboard from './components/transport/TransportDashboard'import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import Auth from './components/Auth';
-import Welcome from './components/Welcome';
-import AdminDashboard from './components/admin/AdminDashboard';
-import TeacherDashboard from './components/teacher/TeacherDashboard';
-import ParentDashboard from './components/parent/ParentDashboard';
-import StudentDashboard from './components/student/StudentDashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import TransportDashboard from './components/transport/TransportDashboard';
 import LibraryDashboard from './components/library/LibraryDashboard';
 import AccountsDashboard from './components/accounts/AccountsDashboard';
@@ -87,74 +75,6 @@ function App() {
               element={
                 <ProtectedRoute requiredRole="accounts">
                   <AccountsDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="/unauthorized" element={<div>Unauthorized Access</div>} />
-          </Routes>
-        </div>
-      </Router>
-    </AuthProvider>
-  );
-}
-
-export default App;
-
-import LibraryDashboard from './components/library/LibraryDashboard';
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/login" element={<Auth />} />
-            <Route 
-              path="/admin/*" 
-              element={
-                <ProtectedRoute requiredRole="admin">
-                  <AdminDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/teacher/*" 
-              element={
-                <ProtectedRoute requiredRole="teacher">
-                  <TeacherDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/parent/*" 
-              element={
-                <ProtectedRoute requiredRole="parent">
-                  <ParentDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/student/*" 
-              element={
-                <ProtectedRoute requiredRole="student">
-                  <StudentDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/transport/*" 
-              element={
-                <ProtectedRoute requiredRole="transport">
-                  <TransportDashboard />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/library/*" 
-              element={
-                <ProtectedRoute requiredRole="library">
-                  <LibraryDashboard />
                 </ProtectedRoute>
               } 
             />
